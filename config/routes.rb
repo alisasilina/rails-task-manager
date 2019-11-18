93a2c2782d47ede060aea124df529523aa2c1832
Alisa Silina <alisasilina@gmail.com>
2019-11-18 16:39:16 +0200

    Done without final refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+  # get 'tasks', to: 'tasks#index'
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/new', to: 'tasks#new'
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
